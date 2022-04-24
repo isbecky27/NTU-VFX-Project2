cdbec16c9178ac1ceec2c4296b0e6379d94c8834
isbecky27 <is.becky27@gmail.com>
2022-04-24 18:08:45 +0800

    modify int to round

--- code/image_blending.py
+++ code/image_blending.py
@@ -16,23 +16,23 @@ def image_blending(src, tar, translation_x, translation_y, percent=0.2):
     th, tw, c = tar.shape
     sh, sw, c = src.shape
 
-    overlap_x = tw - int(translation_x)
-    blend_range_x = int(overlap_x * percent)
-    tend_x, sstart_x = tw - int(overlap_x / 2), int(overlap_x / 2)
+    overlap_x = tw - round(translation_x)
+    blend_range_x = round(overlap_x * percent)
+    tend_x, sstart_x = tw - round(overlap_x / 2), round(overlap_x / 2)
     # crop image
     crop_src = False 
     if th-abs(translation_y) < sh or th == sh:
         crop_src = True
     if translation_y < 0: # shift tar up
         if crop_src:
-            src = src[:int(translation_y)]
+            src = src[:round(translation_y)]
         sh, sw, c = src.shape
-        tar = tar[-int(translation_y):sh-int(translation_y)]
+        tar = tar[-round(translation_y):sh-round(translation_y)]
     else:
         if crop_src:
-            src = src[int(translation_y):]
+            src = src[round(translation_y):]
         sh, sw, c = src.shape
-        tar = tar[sh-int(translation_y):-int(translation_y)]
+        tar = tar[sh-round(translation_y):-round(translation_y)] # error!
 
     # shift
     blend_img = np.concatenate((tar[:, :tend_x], src[:, sstart_x:]), axis=1)
