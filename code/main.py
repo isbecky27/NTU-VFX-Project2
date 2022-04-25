070f836364805378ccf7898aad74e0f0e25254ae
isbecky27 <is.becky27@gmail.com>
2022-04-25 14:29:13 +0800

    add argument in main.py

--- code/main.py
+++ code/main.py
@@ -38,15 +38,15 @@ if __name__ == '__main__':
 
     ## add argument
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_path', type = str, default = '../data/parrington/', help = 'Path to the directory that contains series of images.')
-    parser.add_argument('--result_path', type = str, default = '../result/parrington/', help = 'Path to the directory that stores all of results.')
-    # parser.add_argument('--series_of_images', type = str, default = 'parrington', help = 'The folder of a series of images that contains images and shutter time file.')
-    parser.add_argument('--focal_length_filename', type = str, default = 'pano.txt', help = 'The name of the file where shutter time information is stored.')
+    parser.add_argument('--data_path', type = str, default = '../data/', help = 'Path to the directory that contains series of images.')
+    parser.add_argument('--result_path', type = str, default = '../result/', help = 'Path to the directory that stores all of results.')
+    parser.add_argument('--series_of_images', type = str, default = 'parrington', help = 'The folder of a series of images that contains images and focal length file.')
+    parser.add_argument('--focal_length_filename', type = str, default = 'pano.txt', help = 'The name of the file where focal length information is stored.')
     args = parser.parse_args()
 
     ## variables
-    path = args.data_path
-    save_path = args.result_path
+    path = os.path.join(args.data_path, args.series_of_images, "")
+    save_path = os.path.join(args.result_path, args.series_of_images, "")
     filename = args.focal_length_filename
     os.makedirs(os.path.dirname(save_path), exist_ok = True)
 
@@ -55,32 +55,31 @@ if __name__ == '__main__':
     imgs, focals = read_imgs_and_focals(path, filename)
 
     ## cylindrical warping
-    # print('Cylindrical Warping...')
-    # imgs = [cylindrical_warping(img, focal) for img, focal in zip(imgs, focals)]
+    print('Cylindrical Warping...')
+    imgs = [cylindrical_warping(img, focal) for img, focal in zip(imgs, focals)]
 
+    print('Feature detection and Feature descriptor...')
     keypts, descriptors = [], []
     for img in imgs:
+        
         ## use Harris Corner Detector to get keypoints
-        print('Feature detection...')
         keypoints, blur_img = harris_corner_detector(img)
 
         ## feature descriptor using SIFT
-        print('Feature descriptor...')
         kp, des = SIFT_descriptor(blur_img, keypoints)
         keypts.append(kp)
         descriptors.append(des)
         # np.save(f'{i}_keypoint.npy', keypts)
         # np.save(f'{i}_descriptor.npy', descriptors)
     
+    print('Feature matching and Image matching...')
     trans_x, trans_y = [], []
     for idx in range(len(imgs)-1):
-
+        
         ## feature matching
-        print('Feature matching...')
         matches = feature_matching(descriptors[idx], descriptors[idx+1])
         
         ## image matching
-        print('Image matching...')
         translation_x, translation_y, good_matches = RANSAC_matches(keypts[idx], keypts[idx+1], matches)
         trans_x.append(translation_x)
         trans_y.append(translation_y)
@@ -90,13 +89,13 @@ if __name__ == '__main__':
     matches = feature_matching(descriptors[0], descriptors[-1])
     last_x, last_y, good_matches = RANSAC_matches(keypts[0], keypts[-1], matches)
     accu_y = np.cumsum(trans_y)
-    dy = 0 #(translation_y - accu_y[-1]) / (len(imgs) - 1)
+    dy = (translation_y - accu_y[-1]) / (len(imgs) - 1)
 
+    ## image blending
+    print('Image blending...')
     output = None
     for idx in range(len(imgs)-1):
        
-        ## image blending
-        print('Image blending...')
         if idx == 0:
             output = linear_blending(imgs[idx], imgs[idx+1], trans_x[idx], round(accu_y[idx] + dy * idx))
         else:
